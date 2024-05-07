6f32f98e5e3764398e4101ccf56877aff952f696
AbhinandanBUI <abhinandansinghtt08@gmail.com>
2024-05-08 00:12:01 +0530

    new add
    new add

--- SRCM.UI/src/app/scrm/pages-menu.ts
+++ SRCM.UI/src/app/scrm/pages-menu.ts
@@ -1,62 +1,45 @@
 import { NbMenuItem } from "@nebular/theme";
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  // {
-  //   title: "School Admin",
-  //   icon: "shopping-cart-outline",
-  //   link: "/scrm/admin/dashboard",
-  //   home: false,
-  // },
   {
-    title: "Dashboard",
-    icon: "grid-outline",
-    link: "/scrm/School/dashboard",
+    title: "SCRM",
+    icon: "shopping-cart-outline",
+    link: "/scrm/admin/dashboard",
     home: true,
   },
+
   {
-    title: "Students",
-    icon: "award-outline",
+    title: "School management",
+    icon: "message-circle-outline",
     children: [
       {
-        title: "All Students",
-        link: "/scrm/School/Students/allstudent",
-        icon: "list-outline",
+        title: "Add school",
+        link: "/pages/extra-components/calendar",
       },
       {
-        title: "Add new",
-        link: "/scrm/School/Students/newstudent",
-        icon: "person-add-outline",
+        title: "Add new account",
+        link: "/pages/extra-components/spinner",
       },
 
       {
-        title: "Manage Families",
-        link: "/scrm/School/Students/managefamilies",
-        icon: "people-outline",
+        title: "Membership",
+        link: "/pages/extra-components/spinner",
+      },
+    ],
+  },
+  {
+    title: "Class",
+    icon: "award-outline",
+    children: [
+      {
+        title: "All Class",
+        link: "/scrm/School/Class/allclass",
+        icon: "list-outline",
       },
       {
-        title: "Active / Inactive",
-        link: "/scrm/School/Students/activeinactive",
-        icon: "person-done-outline",
-      },
-      {
-        title: "Admission Letter",
-        link: "/scrm/School/Students/admissionletter",
-        icon: "file-text-outline",
-      },
-      {
-        title: "Student ID Card",
-        link: "/scrm/School/Students/studentcard",
-        icon: "person-outline",
-      },
-      {
-        title: "Print Basic List",
-        link: "/scrm/School/Students/printbasiclist",
-        icon: "download-outline",
-      },
-      {
-        title: "Promote Students",
-        link: "/scrm/School/Students/promotestudent",
-        icon: "trending-up-outline",
+        title: "New Class",
+        link: "/scrm/School/Class/newclass",
+        icon: "person-add-outline",
       },
     ],
   },
