4548b8ad0bea4afef0dc089fa74b82d29460b477
AbhinandanBUI <abhinandansinghtt08@gmail.com>
2024-05-08 00:41:15 +0530

    routing
    routing

--- SRCM.UI/src/app/scrm/scrm-school/scrm-school-routing.module.ts
+++ SRCM.UI/src/app/scrm/scrm-school/scrm-school-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { NotFoundComponent } from "../../pages/miscellaneous/not-found/not-found.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { ScrmSchoolComponent } from "./scrm-school.component";
+
 const routes: Routes = [
   {
     path: "",
